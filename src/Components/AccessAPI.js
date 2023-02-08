c4e0da32956a7e6267dfabd3ff7c635275245b94
Trygve Horn <trygvehorn@gmail.com>
2023-02-08 19:41:34 +0100

    ss

---
+++ src/Components/AccessAPI.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const [url, setUrl] = useState(https://api.imgflip.com/get_memes)
+
+export default const AccessAPI  = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        console.log(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.log(`Error: ${error}`);
+    }
+  };
