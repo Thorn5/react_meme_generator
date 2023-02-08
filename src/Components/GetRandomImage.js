2c25b099c1f8be36296ea732000d8ef137b85ee1
Trygve Horn <trygvehorn@gmail.com>
2023-02-08 22:10:01 +0100

    sd

--- src/Components/GetRandomImage.js
+++ src/Components/GetRandomImage.js
@@ -1,12 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-const GetRandomImage = ({data}) => {
-    console.log(data);
+const GetRandomImage = ({ data }) => {
+    // Weird!
+    // console.log(data);
     // console.log(data.data.memes.length);
-    
+
+    const handleClick = () => {
+        const [arrayLength, setArrayLength] = useState(data.data.memes.length)
+        //add code to generate random image
+    }
+
     return (
         <div>
-        <button>Randomise Meme Image</button>
+            <button onClick={handleClick}>Randomise Meme Image</button>
         </div>
     )
 }
