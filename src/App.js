3df48bf31c18c5a073ab2ec5b68a59d53a7c6d93
Trygve Horn <110484917+Thorn5@users.noreply.github.com>
2023-02-08 19:19:38 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h3>Main Branch</h3>
-    <p>This app was created using a clean bootstrapped react template.</p>
-    <p>The stock react App.js code has been removed.</p>
+    <h3>Add-Meme-Pics Branch</h3>
     </div>
   );
 }
