defec03f5821021e9e2c47f981cd6243d17b8cf5
Trygve Horn <trygvehorn@gmail.com>
2023-02-09 18:02:32 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SetMemeText from "./Components/SetMemeText";
 import RenderTextOverImage from "./Components/RenderTextOverImage";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [apiData, setApiData] = useState(null);
   const [memeCurrentVal, setMemeCurrentVal] = useState(0)
   const [memeMaxVal, setMemeMaxVal] = useState(null)
   const [topMemeText, setTopMemeText] = useState("Top Text")
@@ -15,8 +15,8 @@ function App() {
   return (
     <div className="App">
       <h3><a href="https://platform.wbscodingschool.com/courses/full-stack-web-app/12948/">Trygve's React Meme Generator</a></h3>
-      <SelectImage data={data} memeCurrentVal={memeCurrentVal} setMemeCurrentVal={setMemeCurrentVal} memeMaxVal={memeMaxVal} /> <br />
-      <AccessAPI data={data} setData={setData} memeCurrentVal={memeCurrentVal} setMemeMaxVal={setMemeMaxVal} />
+      <SelectImage memeCurrentVal={memeCurrentVal} setMemeCurrentVal={setMemeCurrentVal} memeMaxVal={memeMaxVal} /> <br />
+      <AccessAPI apiData={apiData} setApiData={setApiData} memeCurrentVal={memeCurrentVal} setMemeMaxVal={setMemeMaxVal} />
       <SetMemeText topMemeText={topMemeText} setTopMemeText={setTopMemeText} bottomMemeText={bottomMemeText} setBottomMemeText={setBottomMemeText} />
       <RenderTextOverImage topMemeText={topMemeText} bottomMemeText={bottomMemeText} />
     </div>
@@ -24,5 +24,3 @@ function App() {
 }
 
 export default App;
-
-
