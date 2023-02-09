89dbeb202b344bec7d0c908e923d84530387a6c7
Trygve Horn <trygvehorn@gmail.com>
2023-02-09 21:34:58 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -7,18 +7,42 @@ import "./App.css";
 
 function App() {
   const [apiData, setApiData] = useState(null);
-  const [memeCurrentVal, setMemeCurrentVal] = useState(0)
-  const [memeMaxVal, setMemeMaxVal] = useState(null)
-  const [topMemeText, setTopMemeText] = useState("Top Text")
-  const [bottomMemeText, setBottomMemeText] = useState("Bottom Text")
+  const [memeCurrentVal, setMemeCurrentVal] = useState(0);
+  const [memeMaxVal, setMemeMaxVal] = useState(null);
+  const [topMemeText, setTopMemeText] = useState("Top Text");
+  const [bottomMemeText, setBottomMemeText] = useState("Bottom Text");
 
   return (
     <div className="App">
-      <h3><a href="https://platform.wbscodingschool.com/courses/full-stack-web-app/12948/">Trygve's React Meme Generator</a></h3>
-      <SelectImage memeCurrentVal={memeCurrentVal} setMemeCurrentVal={setMemeCurrentVal} memeMaxVal={memeMaxVal} /> <br />
-      <AccessAPI apiData={apiData} setApiData={setApiData} memeCurrentVal={memeCurrentVal} setMemeMaxVal={setMemeMaxVal} />
-      <RenderTextOverImage apiData={apiData} memeCurrentVal={memeCurrentVal} topMemeText={topMemeText} bottomMemeText={bottomMemeText} />
-      <SetMemeText topMemeText={topMemeText} setTopMemeText={setTopMemeText} bottomMemeText={bottomMemeText} setBottomMemeText={setBottomMemeText} />
+      <h3>
+        <a href="https://platform.wbscodingschool.com/courses/full-stack-web-app/12948/">
+          Trygve's React Meme Generator
+        </a>
+      </h3>
+      <SelectImage
+        memeCurrentVal={memeCurrentVal}
+        setMemeCurrentVal={setMemeCurrentVal}
+        memeMaxVal={memeMaxVal}
+      />{" "}
+      <br />
+      <AccessAPI
+        apiData={apiData}
+        setApiData={setApiData}
+        memeCurrentVal={memeCurrentVal}
+        setMemeMaxVal={setMemeMaxVal}
+      />
+      <RenderTextOverImage
+        apiData={apiData}
+        memeCurrentVal={memeCurrentVal}
+        topMemeText={topMemeText}
+        bottomMemeText={bottomMemeText}
+      />
+      <SetMemeText
+        topMemeText={topMemeText}
+        setTopMemeText={setTopMemeText}
+        bottomMemeText={bottomMemeText}
+        setBottomMemeText={setBottomMemeText}
+      />
     </div>
   );
 }
--- src/Components/SetMemeText.js
+++ src/Components/SetMemeText.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./RenderTextOverImage.css"
+import "./RenderTextOverImage.css";
 
 const SetMemeText = ({
   topMemeText,
@@ -7,35 +7,39 @@ const SetMemeText = ({
   bottomMemeText,
   setBottomMemeText,
 }) => {
+  const ResetText = () => {
+    setTopMemeText("Top Text");
+    setBottomMemeText("Bottom Text");
+  };
 
-
-return (
-  <div>
-    <b>Please type the desired text:</b>
-    <form>
-      <label>
-        Top Text:
-        <input
-          type="text"
-          name="topText"
-          placeholder={topMemeText}
-          onChange={(e) => setTopMemeText(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Bottom Text:
-        <input
-          type="text"
-          name="bottomText"
-          placeholder={bottomMemeText}
-          onChange={(e) => setBottomMemeText(e.target.value)}
-        />
-      </label>
-      <br />
-    </form>
-  </div>
-);
+  return (
+    <div>
+      <b>Please type the desired text:</b>
+      <form>
+        <label>
+          Top Text:
+          <input
+            type="text"
+            name="topText"
+            placeholder={topMemeText}
+            onChange={(e) => setTopMemeText(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Bottom Text:
+          <input
+            type="text"
+            name="bottomText"
+            placeholder={bottomMemeText}
+            onChange={(e) => setBottomMemeText(e.target.value)}
+          />
+        </label>
+        <br />
+      </form>
+      <button onClick={ResetText}>Reset Text</button>
+    </div>
+  );
 };
 
 export default SetMemeText;
