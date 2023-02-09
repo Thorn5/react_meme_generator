21f53cc870d7cc17da3e4eac021292432e1c7dd2
Trygve Horn <trygvehorn@gmail.com>
2023-02-09 17:51:55 +0100

    ss

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
+import SelectImage from "./Components/SelectImage";
 import AccessAPI from "./Components/AccessAPI";
 import SetMemeText from "./Components/SetMemeText";
-import SelectImage from "./Components/SelectImage";
+import RenderTextOverImage from "./Components/RenderTextOverImage";
 
 function App() {
   const [data, setData] = useState(null);
   const [memeCurrentVal, setMemeCurrentVal] = useState(0)
   const [memeMaxVal, setMemeMaxVal] = useState(null)
+  const [topMemeText, setTopMemeText] = useState("Top Text")
+  const [bottomMemeText, setBottomMemeText] = useState("Bottom Text")
 
   return (
     <div className="App">
       <h3><a href="https://platform.wbscodingschool.com/courses/full-stack-web-app/12948/">Trygve's React Meme Generator</a></h3>
       <SelectImage data={data} memeCurrentVal={memeCurrentVal} setMemeCurrentVal={setMemeCurrentVal} memeMaxVal={memeMaxVal} /> <br />
       <AccessAPI data={data} setData={setData} memeCurrentVal={memeCurrentVal} setMemeMaxVal={setMemeMaxVal} />
-      <SetMemeText />
+      <SetMemeText topMemeText={topMemeText} setTopMemeText={setTopMemeText} bottomMemeText={bottomMemeText} setBottomMemeText={setBottomMemeText} />
+      <RenderTextOverImage topMemeText={topMemeText} bottomMemeText={bottomMemeText} />
     </div>
   );
 }
--- src/Components/SetMemeText.js
+++ src/Components/SetMemeText.js
@@ -1,13 +1,38 @@
 import React, { useState, useEffect } from "react";
 
-const SetMemeText = () => {
-  const [memeTopText, setMemeTopText] = useState("")
-  return(
-    <div>
-      <b>Please type the top and bottom text:</b>
+const SetMemeText = ({
+  topMemeText,
+  setTopMemeText,
+  bottomMemeText,
+  setBottomMemeText,
+}) => {
 
+  return (
+    <div>
+      <b>Please type the desired text:</b>
+      <form>
+        <label>
+          Top Text: 
+          <input
+            type="text"
+            name="topText"
+            placeholder={topMemeText}
+            onChange={(e) => setTopMemeText(e.target.value)}
+            />
+        </label>
+        <br/>
+        <label>
+          Bottom Text: 
+          <input
+            type="text"
+            name="bottomText"
+            placeholder={bottomMemeText}
+            onChange={(e) => setBottomMemeText(e.target.value)}
+            />
+        </label>
+      </form>
     </div>
-  )
+  );
 };
 
 export default SetMemeText;
